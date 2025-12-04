2d679dae7c0f8adf88d51bcfdfc578660be08b90
Luke Gareth Ribchester <luke@luke.gr>
2025-12-04 11:37:55 +0100

    feat(gcp): create the `org_policy` module
    Signed-off-by: Luke Gareth Ribchester <luke@luke.gr>

---
+++ modules/org_policy/main.tf
@@ -0,0 +1,15 @@
+resource "google_project_service" "org_policy" {
+  project = var.project
+  service = "orgpolicy.googleapis.com"
+}
+
+resource "google_org_policy_policy" "this" {
+  name   = "${var.parent}/policies/${var.name}"
+  parent = var.parent
+
+  spec {
+    rules {
+      enforce = var.enforce
+    }
+  }
+}
---
+++ modules/org_policy/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "parent" {
+  type = string
+}
+
+variable "enforce" {
+  type = string
+}
